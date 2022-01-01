91ca3656d873771880938adad99fff58e8da9376
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-01 17:04:59 +0200

    Create addTask function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const { google } = require('googleapis');
 
 const TOKEN_PATH = 'token.json';
+const MY_TASKS_ID = 'MTEyODExNzYzMDMyNzY0NTczNTg6MDow';
 
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.error(`Error loading client secret file: ${err}`);
@@ -19,47 +20,44 @@ const authorize = (credentials, callback) => {
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return console.error(`Error authorizing token: ${err}`);
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    callback(oAuth2Client, MY_TASKS_ID);
   });
 };
 
-const listMyTasks = (auth) => {
-  const TASKLIST_ID = 'MTEyODExNzYzMDMyNzY0NTczNTg6MDow';
+const listMyTasks = (auth, taskListId) => {
   const service = google.tasks({ version: 'v1', auth });
 
   service.tasks.list(
     {
-      tasklist: TASKLIST_ID
+      tasklist: taskListId
     },
     (err, res) => {
       if (err) return console.error(`The API returned an error: ${err}`);
       const tasks = res.data.items;
       if (tasks) {
-        console.log(`Tasks from ${TASKLIST_ID}:`);
         tasks.forEach((task) => {
-          console.log(`${task.title} (${task.id})`);
+          console.log(task.title);
         });
       } else {
         console.log('No tasks found.');
       }
     }
   );
+};
 
-  // service.tasklists.list(
-  //   {
-  //     maxResults: 10
-  //   },
-  //   (err, res) => {
-  //     if (err) return console.error(`The API returned an error: ${err}`);
-  //     const taskLists = res.data.items;
-  //     if (taskLists) {
-  //       console.log('Task lists:');
-  //       taskLists.forEach((taskList) => {
-  //         console.log(`${taskList.title} (${taskList.id})`);
-  //       });
-  //     } else {
-  //       console.log('No task lists found.');
-  //     }
-  //   }
-  // );
+const addTask = (auth, taskListId, taskTitle) => {
+  const service = google.tasks({ version: 'v1', auth });
+
+  service.tasks.insert(
+    {
+      tasklist: taskListId,
+      resource: {
+        title: taskTitle
+      }
+    },
+    (err, res) => {
+      if (err) return console.error(err);
+      console.log('Added task');
+    }
+  );
 };
